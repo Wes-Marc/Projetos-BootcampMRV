79b61d1c2b6e7aaa15cc1919fe2fc8e2d2a5e4fc
Wes-Marc <weslei047@gmail.com>
2021-03-15 15:02:29 -0300

    Construindo o namespace e estrutura base

---
+++ Dio.Series/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dio.Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serie minhasSeries = new Serie();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
